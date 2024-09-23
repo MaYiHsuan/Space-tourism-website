efd827a549b3d1ca660bb2679176911636c197d2
馬苡瑄 <cindyma@mayixuande-MacBook-Pro.local>
2024-09-24 01:10:01 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path';
-import ghPages from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ghPages()],
   base: '/Space-tourism-website/',
   resolve: {
     alias: {
